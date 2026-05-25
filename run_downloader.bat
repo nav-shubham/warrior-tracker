d0cd11bd98a9cc733dcf21cb868d45b0090631f8
Shubham Kadam <subhamkadam2001@gmail.com>
2026-05-25 16:40:02 +0530

    Removed bat launcher

--- run_downloader.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Navigate to the project directory
-cd /d "D:\PythonTools\age"
-
-:: Activate the virtual environment and run the script
-"D:\PythonTools\age\venv\Scripts\python.exe" age.py
-
